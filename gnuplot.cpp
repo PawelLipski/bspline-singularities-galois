a3cea48c64acc5de0b787628e84f2c4be013c013
Pawel Lipski <pawel.p.lipski@gmail.com>
2015-11-12 23:24:00 +0100

    Tidy up function defs

--- gnuplot.cpp
+++ gnuplot.cpp
@@ -7,10 +7,8 @@ int line_no = 1;
 void print_config() {
 	int size = 32;
 	cout << "unset border; unset xtics; unset ytics; unset ztics; set key off" << endl;
-	//cout << "set dgrid3d " << size << "," << size << endl;
 	cout << "set isosamples 50" << endl;
 	cout << "set xrange [0:" << size << "]; set yrange [0:" << size << "]" << endl;
-	//cout << "set pm3d interpolate 0,0" << endl;
 }
 
 void print_grid_line(int x1, int y1, int x2, int y2) {
@@ -24,44 +22,29 @@ double interpolate(double from, double to, int index, int interval_cnt) {
 	return from + (to - from) / interval_cnt * index;
 }
 
-double function(double x, double y) {
-	return x * y;
-}
-
-void sample_region(int l, int r, int u, int d, int sample_cnt, const char* data_file) {
-	int interval_cnt = sample_cnt - 1;
-	ofstream fout(data_file);
-	for (int xi = 0; xi < sample_cnt; xi++) {
-		double x = interpolate(l, r, xi, interval_cnt);
-		for (int yi = 0; yi < sample_cnt; yi++) {
-			double y = interpolate(u, d, yi, interval_cnt);
-			fout << x << " " << y << " " << function(x, y) << endl;
-		}
-	}
-	fout.close();
-}
-
 int function_no = 1;
 
-void print_placeholder_function(int l, int r, int u, int d) {
-	cout << "empty(x,y) = "
+void print_function(const string& name, const string& expr_if_defined, const string& expr_if_undefined, int l, int r, int u, int d) {
+	cout << name << "(x,y) = "
 			<< l << " <= x && x <= " << r << " && "
-			<< u << " <= y && y <= " << d << " ? 0 : 1/0" << endl;
+			<< u << " <= y && y <= " << d << " ? " << expr_if_defined << " : " << expr_if_undefined << endl;
 }
 
-void print_function_over(int l, int r, int u, int d) {
-	cout << "f" << function_no << "(x,y) = "
-			<< l << " <= x && x <= " << r << " && "
-			<< u << " <= y && y <= " << d << " ? x * (16 - x) * y * (16 - y) : empty(x,y)" << endl;
+void print_placeholder_function(int l, int r, int u, int d) {
+	print_function("empty", "0", "1/0", l, r, u, d);
+}
+
+void print_numbered_function(int l, int r, int u, int d) {
+	print_function("f" + to_string(function_no), "x * (16 - x) * y * (16 - y)", "empty(x,y)", l, r, u, d);
 	function_no++;
 }
 
-void print_function_defs() {
+void print_all_functions() {
 	print_placeholder_function(0, 16, 0, 16);
-	print_function_over(0, 8, 0, 8);
-	print_function_over(0, 8, 8, 16);
-	print_function_over(8, 16, 0, 8);
-	print_function_over(8, 16, 8, 16);
+	print_numbered_function(0, 8, 0, 8);
+	print_numbered_function(0, 8, 8, 16);
+	print_numbered_function(8, 16, 0, 8);
+	print_numbered_function(8, 16, 8, 16);
 }
 
 string sum_of_functions(int cnt) {
@@ -87,7 +70,7 @@ int main() {
 		print_grid_line(right, down, left,  down);
 		print_grid_line(left,  down, left,  up);
 	}
-	print_function_defs();
+	print_all_functions();
 	print_splot_command();
 }
 
