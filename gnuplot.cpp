0fdaf7d3708bbfbb7e1523a7a09fa64be2b6e748
Pawel Lipski <pawel.p.lipski@gmail.com>
2015-11-12 23:32:47 +0100

    Prepare for B-spline definition

--- gnuplot.cpp
+++ gnuplot.cpp
@@ -1,8 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 using namespace std;
-
-int line_no = 1;
 
 void print_config() {
 	int size = 32;
@@ -12,30 +11,42 @@ void print_config() {
 }
 
 void print_grid_line(int x1, int y1, int x2, int y2) {
+	static int line_no = 1;
 	cout << "set arrow " << line_no << " from "
 			<< x1 << "," << y1 << ",0 to "
 			<< x2 << "," << y2 << ",0 nohead" << endl;
 	line_no++;
 }
 
-double interpolate(double from, double to, int index, int interval_cnt) {
-	return from + (to - from) / interval_cnt * index;
+
+string bspline_symbolic(const vector<double>& nodes, const string& var) {
+	return "";
 }
 
-int function_no = 1;
+string bspline_symbolic_2d(const vector<double>& x_nodes, const vector<double>& y_nodes, int x_elem, int y_elem) {
+	return "(" + bspline_symbolic(x_nodes, "x") + ") * (" + bspline_symbolic(y_nodes, "y") + ")";
+}
 
-void print_function(const string& name, const string& expr_if_defined, const string& expr_if_undefined, int l, int r, int u, int d) {
+
+void print_function(const string& name, const string& expr, int l, int r, int u, int d) {
 	cout << name << "(x,y) = "
 			<< l << " <= x && x <= " << r << " && "
-			<< u << " <= y && y <= " << d << " ? " << expr_if_defined << " : " << expr_if_undefined << endl;
+			<< u << " <= y && y <= " << d << " " << expr << endl;
+}
+
+void print_conditional_function(
+		const string& name, const string& expr_if_defined, const string& expr_if_undefined,
+		int l, int r, int u, int d) {
+		print_function(name, "? " + expr_if_defined + " : " + expr_if_undefined, l, r, u, d);
 }
 
 void print_placeholder_function(int l, int r, int u, int d) {
-	print_function("empty", "0", "1/0", l, r, u, d);
+	print_conditional_function("empty", "0", "1/0", l, r, u, d);
 }
 
 void print_numbered_function(int l, int r, int u, int d) {
-	print_function("f" + to_string(function_no), "x * (16 - x) * y * (16 - y)", "empty(x,y)", l, r, u, d);
+	static int function_no = 1;
+	print_conditional_function("f" + to_string(function_no), "x * (16 - x) * y * (16 - y)", "empty(x,y)", l, r, u, d);
 	function_no++;
 }
 
