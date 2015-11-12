3b9d5c341b60f7b7f9f9e826075b340734d00d4f
Pawel Lipski <pawel.p.lipski@gmail.com>
2015-11-12 23:12:40 +0100

    Render sample function over the mesh

--- gnuplot.cpp
+++ gnuplot.cpp
@@ -1,12 +1,16 @@
+#include <fstream>
 #include <iostream>
 using namespace std;
 
 int line_no = 1;
 
-void print_header() {
+void print_config() {
 	int size = 32;
 	cout << "unset border; unset xtics; unset ytics; unset ztics; set key off" << endl;
+	//cout << "set dgrid3d " << size << "," << size << endl;
+	cout << "set isosamples 50" << endl;
 	cout << "set xrange [0:" << size << "]; set yrange [0:" << size << "]" << endl;
+	//cout << "set pm3d interpolate 0,0" << endl;
 }
 
 void print_grid_line(int x1, int y1, int x2, int y2) {
@@ -16,14 +20,65 @@ void print_grid_line(int x1, int y1, int x2, int y2) {
 	line_no++;
 }
 
-void print_footer() {
-	cout << "splot 256**2-(abs(x-16)*abs(y-16))**2; pause 10" << endl;
+double interpolate(double from, double to, int index, int interval_cnt) {
+	return from + (to - from) / interval_cnt * index;
+}
+
+double function(double x, double y) {
+	return x * y;
+}
+
+void sample_region(int l, int r, int u, int d, int sample_cnt, const char* data_file) {
+	int interval_cnt = sample_cnt - 1;
+	ofstream fout(data_file);
+	for (int xi = 0; xi < sample_cnt; xi++) {
+		double x = interpolate(l, r, xi, interval_cnt);
+		for (int yi = 0; yi < sample_cnt; yi++) {
+			double y = interpolate(u, d, yi, interval_cnt);
+			fout << x << " " << y << " " << function(x, y) << endl;
+		}
+	}
+	fout.close();
+}
+
+int function_no = 1;
+
+void print_placeholder_function(int l, int r, int u, int d) {
+	cout << "empty(x,y) = "
+			<< l << " <= x && x <= " << r << " && "
+			<< u << " <= y && y <= " << d << " ? 0 : 1/0" << endl;
+}
+
+void print_function_over(int l, int r, int u, int d) {
+	cout << "f" << function_no << "(x,y) = "
+			<< l << " <= x && x <= " << r << " && "
+			<< u << " <= y && y <= " << d << " ? x * (16 - x) * y * (16 - y) : empty(x,y)" << endl;
+	function_no++;
+}
+
+void print_function_defs() {
+	print_placeholder_function(0, 16, 0, 16);
+	print_function_over(0, 8, 0, 8);
+	print_function_over(0, 8, 8, 16);
+	print_function_over(8, 16, 0, 8);
+	print_function_over(8, 16, 8, 16);
+}
+
+string sum_of_functions(int cnt) {
+	string res = "f1(x,y)";
+	for (int i = 2; i <= cnt; i++)
+		res += " + f" + to_string(i) + "(x,y)";
+	return res;
+}
+
+void print_splot_command() {
+	cout << "splot " << sum_of_functions(4) << " with lines; pause 10" << endl;
 }
 
 int main() {
 	int N;
 	cin >> N;
-	print_header();
+	print_config();
 	for (int i = 0; i < N; i++) {
 		int left, right, up, down;
 		cin >> left >> right >> up >> down;
@@ -32,6 +87,7 @@ int main() {
 		print_grid_line(right, down, left,  down);
 		print_grid_line(left,  down, left,  up);
 	}
-	print_footer();
+	print_function_defs();
+	print_splot_command();
 }
 
