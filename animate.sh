553b87cc8a5c101630a08f08fe249329fcdf2ef5
Pawel Lipski <pawel.p.lipski@gmail.com>
2016-05-11 15:09:05 +0200

    Animate B-spline overview

--- animate.sh
+++ animate.sh
@@ -1,7 +1,25 @@
 rm -rf eps/
 mkdir -p eps gif
+
+if false; then
 for i in `seq 0 5 355`; do
 	echo $i
 	./render-non-rect-support non-rect-support-`printf "%.3i" $i` $i | gnuplot
 done
 convert -delay 10 -density 150 -loop 0 -resize 1024x768 eps/non-rect-support-*.eps gif/non-rect-support.gif
+fi
+
+overview() {
+	tag=$1
+	shift
+	for i in `seq 0 5 120`; do
+		echo $tag $i
+		i0=`printf "%.3i" $i`
+		./render-bsplines bsplines-overview-$tag-$i0 $i Regular $@ | gnuplot
+	done
+	convert -delay 10 -density 150 -loop 1 -resize 1024x768 eps/bsplines-overview-$tag-*.eps gif/bsplines-overview-${tag}.gif
+}
+
+overview part1 1 9 18 21
+overview part2 36 41 44
+
--- render-bsplines.cpp
+++ render-bsplines.cpp
@@ -6,9 +6,17 @@ using namespace std;
 #include "bspline-non-rect.h"
 #include "gnuplot.h"
 
-int SAMPLE_CNT = 31; // in each dimension
+int SAMPLE_CNT = 41; // in each dimension
 
+int hardcoded_colors[] = { 1, 9, 18, 21, 36, 41, 44 };
 string colors[] = { "turquoise", "green", "red", "navy", "magenta", "blue", "orange" };
+
+int find_hardcoded_color(int index) {
+	for (unsigned i = 0; i < sizeof(hardcoded_colors) / sizeof(int); i++)
+		if (hardcoded_colors[i] == index)
+			return i;
+	return 0;
+}
 
 void plot(const Function2D& f, const Rect& area, string color, int tag = 1) {
 	static bool replot = false;
@@ -29,20 +37,20 @@ int main(int argc, char** argv) {
 
 	int size = generate_and_render_grid(3);
 	print_config(size, SAMPLE_CNT);
-	print_rotate_view(60, 45);
+	print_rotate_view(60, atoi(argv[2]));
 	if (output == EPS)
 		print_eps_terminal(argv[1]);
 
-	string type = argv[2]; // Regular or Gnomon
+	string type = argv[3]; // Regular or Gnomon
 	NurbsOverAdaptedGrid nurbs(3);
-	int no = 0;
-	for (int i = 3; i < argc; i++) {
+	for (int i = 4; i < argc; i++) {
 		string file = string() + "bspline-" + argv[i] + ".dat";
 		int index = atoi(argv[i]);
 		Rect support = nurbs.get_bspline_support(index);
 		if (type == "Regular") {
 			samples_2d(nurbs.get_bspline(index), support, file, SAMPLE_CNT);
-			print_plot_command(file, colors[no++], i > 3);
+			int color_index = find_hardcoded_color(index);
+			print_plot_command(file, colors[color_index], i > 4);
 		} else {
 			const GnomonNurbs& gb = dynamic_cast<const GnomonNurbs&>(nurbs.get_bspline(index));
 			plot(gb.get_trunk(), gb.get_trunk_support_1(), "red", 1);
