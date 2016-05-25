aae20645d6b1846c030f349b83c393f8e70911d3
Pawel Lipski <pawel.p.lipski@gmail.com>
2016-05-25 23:44:33 +0200

    Smooth out flops estimate plots

--- flops-estimates/compute-flops.sh
+++ flops-estimates/compute-flops.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# usage, for order=3: ./compute-flops.sh 3
-
 rm -rf tmp
 mkdir tmp
 
-order=$1
 max_depth=32
-generate_cmd=${2-../generate --galois --edged-4}
-output_prefix=${3-flops-generate}
+generate_cmd=${1-../generate --galois}
+output_prefix=${2-flops-generate}
 
 for depth in `seq $max_depth`; do
 	echo $depth
-    $generate_cmd $depth $order > tmp/mesh-$depth
-
-
+    $generate_cmd $depth > tmp/mesh-$depth
     analyser -f tmp/mesh-$depth > tmp/flops-$depth
     cat tmp/flops-$depth | awk 'BEGIN { max=0 } { sum+=$3; if ($1>max) max=$1 } END {print max+1, sum}' >> tmp/total-flops
 done
@@ -38,14 +33,15 @@ a_x__b() {
 plot() {
 gnuplot << EOF
 	set terminal png
-	set output '${output_prefix}-${output_suffix}-${order}-order.png'
+	set output '${output_prefix}-${output_suffix}.png'
 	fit(x) = $fit_fun
 	$fit_init
 	fit fit(x) 'tmp/total-flops' via $fit_via
 	set xlabel 'N'
 	set ylabel 'Flops'
-	plot 'tmp/total-flops' w l title 'measured flops(N)', \
-		fit(x) title sprintf($fit_fun_sprintf) w l
+	set key left top
+	plot 'tmp/total-flops' with points title 'measured flops(N)', \
+		fit(x) title sprintf($fit_fun_sprintf) with lines
 EOF
 }
 
@@ -53,6 +49,4 @@ x__b
 plot
 a_x__b
 plot
-
-#eog ${output_prefix}-*.png
 
