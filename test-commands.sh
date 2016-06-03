1770409ae2ccd55e48f2f0bb04decdc2fca12d1d
Pawel Lipski <pawel.p.lipski@gmail.com>
2016-06-03 16:28:22 +0200

    Update test commands

--- test-commands.sh
+++ test-commands.sh
@@ -1,11 +1,14 @@
 
 opts="draw-neighbors draw-plain draw-supports galois gnuplot"
+shapes="q r"
 depths="`seq 1 5`"
 
 for opt in $opts; do
-	echo "./generate --$opt #${opt}_no-depth"
-	for depth in $depths; do
-		echo "./generate --$opt $depth #${opt}_depth-${depth}"
+	for shape in $shapes; do
+		echo "./generate --$opt -$shape #${opt}_no-depth_$shape"
+		for depth in $depths; do
+			echo "./generate --$opt -$shape $depth #${opt}_depth-${depth}_$shape"
+		done
 	done
 done
 
